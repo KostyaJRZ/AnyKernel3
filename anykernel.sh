4f51a4f582fe695df3df86fac1daab14781ae13a
KostyaJRZ <jrzroyale@gmail.com>
2020-08-23 16:59:27 +0530

    AK3: Initial changes for arubaslim

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=Alpaca
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
+device.name1=arubaslim
+device.name2=arubaslimxx
 device.name3=
 device.name4=
 device.name5=
@@ -20,7 +20,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=;
+block=/dev/block/mmcblk0p8;
 is_slot_device=0;
 ramdisk_compression=auto;
 
